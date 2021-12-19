960578cc7dcc647077141c353d0f656c8c8a892d
muhammad amin <92498377+Sangarya@users.noreply.github.com>
2021-12-20 06:40:40 +0800

    Update usernew.sh

--- sshvpn/usernew.sh
+++ sshvpn/usernew.sh
@@ -10,66 +10,33 @@ white='\x1b[37m'
 bold='\033[1m'
 off='\x1b[m'
 flag='\x1b[47;41m'
-MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
-clear
-# DELETE AKUN EXPIRED
-               hariini=`date +%Y-%m-%d`
-               cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
-               totalaccounts=`cat /tmp/expirelist.txt | wc -l`
-               for((i=1; i<=$totalaccounts; i++ ))
-               do
-               tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
-               username=`echo $tuserval | cut -f1 -d:`
-               userexp=`echo $tuserval | cut -f2 -d:`
-               userexpireinseconds=$(( $userexp * 86400 ))
-               tglexp=`date -d @$userexpireinseconds`             
-               tgl=`echo $tglexp |awk -F" " '{print $3}'`
-               while [ ${#tgl} -lt 2 ]
-               do
-               tgl="0"$tgl
-               done
-               while [ ${#username} -lt 15 ]
-               do
-               username=$username" " 
-               done
-               bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
-               echo "echo "Expired- User : $username Expire at : $tgl $bulantahun"" >> /usr/local/bin/alluser
-               todaystime=`date +%s`
-               if [ $userexpireinseconds -ge $todaystime ] ;
-               then
-		    	:
-               else
-               
-               userdel $username
-               fi
-               done
-# DELETE
-
+MYIP=$(wget -qO- ipv4.icanhazip.com);
 ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 CITY=$(curl -s ipinfo.io/city )
 COUNTRY=$(curl -s ipinfo.io/country )
-
-#MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 read -p "Username : " Login
 read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
-
-source /var/lib/premium-script/ipvps.conf
-if [[ "$IP" = "" ]]; then
-domain=$(cat /etc/v2ray/domain)
-else
-domain=$IP
-fi
-IP=$(wget -qO- ifconfig.co);
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+IP=$(wget -qO- icanhazip.com);
+domain=$(cat /etc/v2ray/domain);
+IP=$(wget -qO- ipinfo.io/ip);
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 sleep 1
-echo "Akun Berhasil Dibuat!"
+echo Ping Host
+echo Cek Hak Akses...
+sleep 0.5
+echo Permission Accepted
+clear
+sleep 0.5
+echo Membuat Akun: $Login
+sleep 0.5
+echo Setting Password: $Pass
+sleep 0.5
 clear
 tgl=$(date -d "$masaaktif days" +"%d")
 bln=$(date -d "$masaaktif days" +"%b")
@@ -80,6 +47,7 @@ bln2=$(date +"%b")
 thn2=$(date +"%Y")
 tnggl="$tgl2 $bln2, $thn2"
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+dibuat=$(date +"%b %C, %Y")
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
@@ -89,6 +57,8 @@ echo -e "Username      : $Login "
 echo -e "Password      : $Pass"
 echo -e "==============================" | lolcat
 echo -e "ISP           : $ISP"
+echo -e "COUNTRY       : $COUNTRY"
+echo -e "CITY          : $CITY"
 echo -e "Host          : $MYIP"
 echo -e "Domain        : $domain"
 echo -e "OpenSSH       : 171, 22"
@@ -124,3 +94,4 @@ echo -e ""
 echo -e " Script Installer By : Geo•NTB"
 echo -e ""
 echo -e "==============================" | lolcat
+
