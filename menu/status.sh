f55059a237bc4e35280059ca7b687e1c1056ec0c
muhammad amin <92498377+Sangarya@users.noreply.github.com>
2021-12-19 20:55:11 +0800

    Update status.sh

--- menu/status.sh
+++ menu/status.sh
@@ -55,8 +55,8 @@ nginx=$(systemctl status nginx | grep -i "active (running)")
 squid=$(systemctl status squid | grep -i "active (running)")
 cron=$(systemctl status cron | grep -i "active (running)")
 fail2ban=$(systemctl status fail2ban | grep -i "active (running)")
-vnstat=$(systemctl status vnstat | grep -i "active (running)")
 ksslh=$(systemctl status sslh | grep -i "active (running)")
+vnstat=$(systemctl status vnstat | grep -i "active (running)")
 ohpp=$(systemctl status dropbear-ohp.service  | grep -i "active (running)")
 ohppp=$(systemctl status ovpn-ohp.service  | grep -i "active (running)")
 ohpppp=$(systemctl status ssh-ohp.service  | grep -i "active (running)")
@@ -373,9 +373,9 @@ echo -e " $mg - $off $bd OpenVPN            $off   : $sovpn "
 echo -e " $mg - $off $bd WebSocket Dropbear $off   : $swsdrop"
 echo -e " $mg - $off $bd WebSocket OpenSSH   $off  : $swsopen"
 echo -e " $mg - $off $bd WebSocket OpenVPN  $off   : $swsovpn"
-echo -e " $mg - $off $bd OHP Dropbear $off       : $sohpp"
-echo -e " $mg - $off $bd OHP Ovpn   $off         : $sohppp"
-echo -e " $mg - $off $bd OHP SSH  $off           : $sohpppp"
+echo -e " $mg - $off $bd OHP Dropbear $off         : $sohpp"
+echo -e " $mg - $off $bd OHP Ovpn   $off           : $sohppp"
+echo -e " $mg - $off $bd OHP SSH  $off             : $sohpppp"
 echo -e " $mg - $off $bd WebSocket TLS    $off     : $swstls"
 echo -e " $mg - $off $bd VMess TLS         $off    : $sv2ray "
 echo -e " $mg - $off $bd VMess NON-TLS    $off     : $sv2none "
