b348f5456c2ecabb0046978261be0e35c38c1837
santzx45 <106920145+santzx45@users.noreply.github.com>
2022-07-16 22:01:37 +0700

    Update cek-xws.sh

--- xray/cek-xws.sh
+++ xray/cek-xws.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/Sangarya/izin/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/santzx45/izin/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
