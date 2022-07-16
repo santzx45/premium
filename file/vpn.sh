aff7969f077cb7a9037f4cf1a498077ca1cdbec4
santzx45 <106920145+santzx45@users.noreply.github.com>
2022-07-16 18:32:54 +0700

    Update vpn.sh

--- file/vpn.sh
+++ file/vpn.sh
@@ -15,7 +15,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/Sangarya/premium/main/file/vpn.zip
+wget https://raw.githubusercontent.com/santzx45/main/file/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
